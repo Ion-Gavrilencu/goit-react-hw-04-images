b3902ae015e724118430999299e9ba50039a69d9
Ion-Gavrilencu <157795149+Ion-Gavrilencu@users.noreply.github.com>
2024-09-14 15:18:18 +0300

    update 03

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Searchbar.module.css';
@@ -20,18 +19,23 @@ const Searchbar = ({ onSubmit }) => {
   return (
     <header className={styles.searchbar}>
       <form className={styles.form} onSubmit={handleSubmit}>
-        <button type="submit" className={styles.button}>
-          <span className={styles.label}>Search</span>
-        </button>
-        <input
-          className={styles.input}
-          type="text"
-          value={query}
-          onChange={handleInputChange}
-          placeholder="Search images and photos"
-          autoComplete="off"
-          autoFocus
-        />
+        <div className={styles.group}>
+          <svg viewBox="0 0 24 24" aria-hidden="true" className={styles.searchIcon}>
+            <g>
+              <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+            </g>
+          </svg>
+          <input
+            id="query"
+            className={styles.input}
+            type="search"
+            placeholder="Search..."
+            value={query}
+            onChange={handleInputChange}
+            autoComplete="off"
+            autoFocus
+          />
+        </div>
       </form>
     </header>
   );
@@ -42,4 +46,5 @@ Searchbar.propTypes = {
 };
 
 export default Searchbar;
+
 
